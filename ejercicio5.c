88b54ce0d6fc2008fd4d8c99080bfcf4f2579508
Seiji314288758 <nietoseiji@gmail.com>
2025-10-28 12:55:55 -0600

    Create ejercicio5.c for number series generation
    Implement a program to generate a series of numbers.

---
+++ ejercicio5.c
@@ -0,0 +1,21 @@
+/* Victor Seiji Nieto Morita
+clase 3 ejercicio 5*/
+
+#include <stdio.h>
+
+void main()
+{
+ int elem, acum = 0;
+  printf("Dame el numero de elementos de la serie");
+  scanf("%d", &elem);
+  
+  for(int i = 0; i < elem; i++)
+    {
+      acum = acum + 5;
+      printf("%d, ", acum);
+    
+    }
+
+    printf("\n");
+     
+}
