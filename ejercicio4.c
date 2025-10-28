3e17b2405cec5cb451d452300c3a5edf9703f6dc
Seiji314288758 <nietoseiji@gmail.com>
2025-10-28 12:54:33 -0600

    Implement number printing for multiples of 3
    Add a program to print numbers divisible by 3 from 1 to 100.

---
+++ ejercicio4.c
@@ -0,0 +1,15 @@
+/*Victor Seiji Nieto Morita
+clase 3 ejercicio 4*/
+
+#include <stdio.h>
+
+void main()
+{
+  for(int i = 1; i <= 100; i++)
+    {
+      if(i % 3 == 0)
+      {
+        printf("Num: %d\n", i);
+      }
+    }
+}
