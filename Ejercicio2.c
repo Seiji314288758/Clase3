28b4adfb30e17426b9d17585b52a0f641cf79617
Seiji314288758 <nietoseiji@gmail.com>
2025-10-31 03:10:56 -0600

    Update Ejercicio2.c

--- Ejercicio2.c
+++ Ejercicio2.c
@@ -1,11 +1,11 @@
 /* Victor Seiji Nieto Morita
-clase, 4 ejercicio 1*/
+clase 3 ejercicio 1*/
 
 #include <stdio.h>
 
 void main()
 {
-  for(int i = 1; i <= 10; i++)
+  for(int i = 1; i <= 100; i++)
     {
       printf("Num: %d\n", i);
     }
