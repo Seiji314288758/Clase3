ffb68abcccde0536f23e3a51e0656d84cb0aa223
Seiji314288758 <nietoseiji@gmail.com>
2025-10-31 01:05:50 -0600

    Create Ejercicio1.c

---
+++ Ejercicio1.c
@@ -0,0 +1,24 @@
+/*Victor Seiji Nieto Morita
+clase 3 ejercicio 1*/
+
+#include <stdio.h>
+#include <math.h>
+
+void main()
+{
+  int x = 0, res = 0;
+
+  printf("Dame un numero: "):
+  scanf("%d", &x);
+
+  if (x <= 0)
+  {
+    res = x + 3;
+  }
+  else
+  {
+    res = pow(x,2) + (x * 2);
+  }
+
+  printf("El resultado es: %d\n", res);
+}
